4f8603f336cbf3022a7e633dd937ef0601f9ffc4
Vidushika0316 <dasanayakasmvt.19@uom.lk>
2022-04-23 00:03:49 +0530

    Created Admin module

---
+++ Backend/demo/src/main/java/com/ecommerce/db/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ecommerce.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
